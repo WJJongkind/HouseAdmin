26a942447b2aeca17a5b39ce62127048dfc34c48
unknown <imjustacowlolz@gmail.com>
2019-04-20 21:47:34 +0200

    documented ResponseParser and JSONConvertable

--- Front-end/HouseAdmin/src/app/BackendCommunication/JSONConvertable.ts
+++ Front-end/HouseAdmin/src/app/BackendCommunication/JSONConvertable.ts
@@ -1,9 +1,24 @@
+/**
+ * Interface to which objects have to adhere that are send with the "BackendRequest" class.
+ */
 export interface JSONConvertable {
+
+    /**
+     * The JSON representation of the object.
+     */
     json: {}
+
 }
 
+/**
+ * Convenience class with which objects of type "{}" can be wrapped and send with "BackendRequest".
+ */
 export class JSONWrapper implements JSONConvertable {
 
+    /**
+     * Wraps JSON in a JSONWrapper.
+     * @param json The JSON that has to be wrapped so that it can be send with "BackendRequest".
+     */
     constructor(public json: {}) { }
 
 }
--- Front-end/HouseAdmin/src/app/BackendCommunication/ResponseParser.ts
+++ Front-end/HouseAdmin/src/app/BackendCommunication/ResponseParser.ts
@@ -1,9 +1,21 @@
-export interface Parser<T> {
+/**
+ * Interface for parsers that can parse a JavaScript object to a TypeScript object.
+ */
+export interface ObjectToTypescriptParser<T> {
     parse(input: T): T
 }
 
-export class ResponseParser<T> implements Parser<T> {
+/**
+ * Class used to parse a non-array JavaScript object to a non-array TypeScript object.
+ */
+export class ResponseParser<T> implements ObjectToTypescriptParser<T> {
+
+    /**
+     * Constructs a ResponseParser.
+     * @param constructor The constructor with which objects of type T can be instantiated.
+     */
     constructor(private constructor: new () => T) { }
+
     parse(input: T): T {
         let output = new this.constructor()
         Object.assign(output, input)
@@ -11,8 +23,15 @@ export class ResponseParser<T> implements Parser<T> {
     }
 }
 
-export class ArrayResponseParser<T> implements Parser<T[]> {
+/**
+ * Class used to parse an array of JavaScript object to an array of TypeScript object.
+ */
+export class ArrayResponseParser<T> implements ObjectToTypescriptParser<T[]> {
 
+    /**
+     * Constructs a ArrayResponseParser.
+     * @param constructor The constructor with which objects of type T can be instantiated.
+     */
     constructor(private typeConstructor: new () => T) { }
 
     parse(input: T[]): T[] {
