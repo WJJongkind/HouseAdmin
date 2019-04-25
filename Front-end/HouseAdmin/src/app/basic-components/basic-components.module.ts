a42cfcd0ebdf2133760bec541ed3a278dea308c7
unknown <imjustacowlolz@gmail.com>
2019-04-25 16:48:04 +0200

    documented CFM and BasicComponentsModule

--- Front-end/HouseAdmin/src/app/basic-components/basic-components.module.ts
+++ Front-end/HouseAdmin/src/app/basic-components/basic-components.module.ts
@@ -4,6 +4,9 @@ import { FinanceTableComponent } from './finance-table/finance-table.component';
 import { RouterModule } from "@angular/router";
 import { FormsModule } from '@angular/forms';
 
+/**
+ * BasicComponentsModule contains all components that are reused in multiple places throughout the application. Components for various purposes are available here.
+ */
 @NgModule({
   imports: [
     CommonModule,
--- Front-end/HouseAdmin/src/app/common/common-functions.module.ts
+++ Front-end/HouseAdmin/src/app/common/common-functions.module.ts
@@ -5,6 +5,10 @@ import { Injectable } from '@angular/core';
 import { LogoutService } from './logout.service';
 import { CookieService } from 'ngx-cookie-service';
 
+/**
+ * The CFM (CommonFunctionsModule) class contains a wide variety of commonly used functions throughout the application. This class it a placeholder and all functions will be segragated into appropriate
+ * classes in the future.
+ */
 @NgModule({
   imports: [
     CommonModule
@@ -15,45 +19,68 @@ import { CookieService } from 'ngx-cookie-service';
   providedIn: 'root'
 })
 export class CFM {
+
   constructor(private logoutService: LogoutService, private cookie: CookieService){}
 
-  public static  getDate = function(daydiff=0, monthdiff=0, yeardiff=0): Date {
+  /**
+   * Obtain the date with the given offset. The offset is calculated from the current date.
+   * @param daydiff The amount of days to offset the returned date from the current date.
+   * @param monthdiff The amount of months to offset the returned date from the current date.
+   * @param yeardiff The amount of years to offset the returned date from the current date.
+   */
+  public static getDate(daydiff: number=0, monthdiff: number=0, yeardiff: number=0): Date {
     var date = new Date();
     date.setDate(date.getDate() + daydiff);
     date.setFullYear(date.getFullYear() + yeardiff);
     date.setMonth(date.getMonth() + monthdiff);
-
-    var year = date.getFullYear();
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
   
-    return new Date(year, month - 1, day);
+    return date;
   }
 
+  /**
+   * Converts a string-representation of a date to a Date object.
+   * @param date The string which has to be converted to a date.
+   */
   public static makeDate(date: string): Date {
     var dateParts = date.split('-');
     var _date = new Date(+dateParts[0], +dateParts[1] - 1, +dateParts[2]); 
     return _date;
   }
 
+  /**
+   * Converts the given Date object to a string-representation formatted as yyyy-mm-dd.
+   * @param date The Date object to be converted to a string.
+   */
   public static makeDateString(date: Date): string {
     var month = (date.getMonth() + 1) + "";
     var day = date.getDate() + "";
 
-    month.length == 1 ? month = "0" + month : month = month
-    day.length == 1 ? day = "0" + day : day = day
+    month = month.length == 1 ? "0" + month : month
+    day = day.length == 1 ? "0" + day : day
 
     return date.getFullYear() + "-" + month + "-" + day;
   }
 
-  static getElementByIDValue(id) {
+  /**
+   * Returns the string-representation of the value of a HTML DOM element with the given ID.
+   * @param id The HTML-id of the element of which the value has to be obtained.
+   */
+  static getElementByIDValue(id: string): string {
     return (<HTMLInputElement>document.getElementById(id)).value;
   }
 
-  static getElementByID(id) {
+  /**
+   * Returns a typescript HTMLInputElement representation of a HTML DOM element with the given ID.
+   * @param id The HTML-id of the element which has to be obtained.
+   */
+  static getElementByID(id: string): HTMLInputElement {
     return (<HTMLInputElement>document.getElementById(id));
   }
 
+  /**
+   * Performs a logout on the current session & redirects the user to the root-path of the given router.
+   * @param router Router used to navigate to the root-path.
+   */
   logout(router: Router) {
     this.logoutService.logout(this.getSessionID(), () => {
       // Do nothing
@@ -64,37 +91,68 @@ export class CFM {
     router.navigateByUrl('')
   }
 
-  getCookie(name: string) {
+  /**
+   * Obtains the value of the cookie with the given name.
+   * @param name Name of the cookie.
+   */
+  getCookie(name: string): string {
     return this.cookie.get(name);
   }
 
+  /**
+   * Deletes any cookies with the given name.
+   * @param name The name of the cookie which has to be deleted.
+   */
   deleteCookie(name: string) {
     this.cookie.delete(name);
     this.cookie.delete(name, "/");
   }
 
+  /**
+   * Sets a new cookie.
+   * @param name The name of the new cookie.
+   * @param value The value of the new cookie.
+   */
   setCookie(name: string, value: string) {
     this.cookie.set(name, value,undefined,"/");
   }
 
+  /**
+   * Deletes all cookies.
+   */
   deleteCookies() {
     this.cookie.deleteAll();
     this.cookie.deleteAll("/");
   }
 
-  getSessionID() {
+  /**
+   * Obtains the ID of the current session, used for back-end communication.
+   */
+  getSessionID(): string {
     return this.getCookie("sessionID");
   }
 
+  /**
+   * Sets the current group with which the user is interacting.
+   * @param id The ID of the group with which the user is interacting.
+   */
   setActivatedGroup(id: string) {
     this.setCookie("groupID", id);
   }
 
+  /**
+   * Obtains the ID of the group with which the user is currently interacting.
+   */
   getActivatedGroup() {
     return this.getCookie("groupID");
   }
 
-  public static deleteFromArray(array, obj) {
+  /**
+   * Removes the given object from the given array and returns the newly created array.
+   * @param array The array from which the object has to be removed.
+   * @param obj The object that needs to be removed.
+   */
+  public static deleteFromArray<T>(array: [T], obj: T): [T] {
     let index = array.indexOf(obj);
 
     if(index > -1) {
@@ -104,10 +162,16 @@ export class CFM {
     return array;
   }
 
-  public static findKeyValueInArray(array: {}[], key, value) {
+  /**
+   * Find & returns the first entry in an array of JSON objects that contains the given value for the given property.
+   * @param array The array in which the entry has to be searched.
+   * @param propertyName The name of the property.
+   * @param value The value which the property has to have.
+   */
+  public static findKeyValueInArray(array: {}[], propertyName, value): {} {
     for(var i = 0; i < array.length; i++) {
       let entry = array[i];
-      if(entry[key] == value) {
+      if(entry[propertyName] == value) {
         return entry;
       }
     }
@@ -115,10 +179,16 @@ export class CFM {
     return null;
   }
 
-  public static sortJSONArrayByProperty(array: {}[], key: string, method: SortMethod) {
+  /**
+   * Sorts a JSON array by the given property with the given method.
+   * @param array The array which has to be sorted.
+   * @param propertyName The name of the property by which the array has to be sorted. 
+   * @param method The sorting-method.
+   */
+  public static sortJSONArrayByProperty(array: {}[], propertyName: string, method: SortMethod) {
     array.sort(function(ra, rb) {
-      var a = ra[key];
-      var b = rb[key];
+      var a = ra[propertyName];
+      var b = rb[propertyName];
 
       switch(method) {
         case SortMethod.ascending: {
@@ -131,8 +201,15 @@ export class CFM {
     })
   }
 
-  public static linkKeyEventOnInputToAction(inputID: string, event: KeyEvents, key: KeyEventKeys, action: () => void) {
-    document.getElementById(inputID)
+  /**
+   * Binds the given action to all HTML elements that have the given id. The action is triggered when the given key is pressed.
+   * @param elementId The ID of the HTML element.
+   * @param event The event for which the action has to be triggered (keyup/keydown).
+   * @param key The key for which the action is triggered.
+   * @param action The action that needs to be executed.
+   */
+  public static linkKeyEventOnInputToAction(elementId: string, event: KeyEvents, key: KeyEventKeys, action: () => void) {
+    document.getElementById(elementId)
     .addEventListener(event, function(event) {
       event.preventDefault();
       if (event.keyCode == key) {
@@ -142,16 +219,25 @@ export class CFM {
   }
 }
 
+/**
+ * Enum containing the supported sorting methods.
+ */
 export enum SortMethod {
   ascending,
   descending
 }
 
+/**
+ * Enum containing the supported KeyEvents for binding actions to key-press events.
+ */
 export enum KeyEvents {
   keyUp = "keyup",
   keyDown = "keydown"
 }
 
+/**
+ * Enum containing the supported keys for binding actinos to key-press events.
+ */
 export enum KeyEventKeys {
   enter = 13
 }
